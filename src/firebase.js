905572c5b687f5340d6450c09d14e9b6824089b2
Michael Fecteau <michaelfecteau18@gmail.com>
2026-06-23 14:43:18 -0400

    Fix Firebase exports

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAikW-opcIzFdyCyOkY9LXfWm0JHRzOc10",
   authDomain: "voyo-554e7.firebaseapp.com",
@@ -16,6 +13,14 @@ const firebaseConfig = {
   measurementId: "G-Y20DLNGMQQ"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+const analytics =
+  typeof window !== "undefined"
+    ? getAnalytics(app)
+    : null;
+
+export { app, analytics };
